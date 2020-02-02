9174e57792620215a118499b29e4142136c7a3b3
li-zh-ou <2401567802@qq.com>
2020-02-02 21:53:13 +0800

    第二次

---
+++ newy2/src/main/java/com/accp/Newy2Application.java
@@ -0,0 +1,13 @@
+package com.accp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Newy2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Newy2Application.class, args);
+	}
+
+}
---
+++ newy2/src/test/java/com/accp/newy2/Newy2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accp.newy2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Newy2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
